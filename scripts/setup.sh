98db37e9da4dae3523f0a2564d29b7f6e8dc1a6a
techyCoder81 <mtklein98@gmail.com>
2023-07-14 09:12:15 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+echo "Setting minimal profile"
+rustup set profile minimal
+
+echo "Defaulting to stable"
+rustup default stable
+
 echo "Installing cargo-skyline."
 cargo install cargo-skyline
 
 echo "Updating skyline std (this may take a minute, with no output)"
 cargo skyline update-std
-
-echo "Updating skyline deps"
 
 echo "Installing python3"
 apt-get update && apt-get install -y python3
